7578ee6977d486bb158105cfd8139ce654e423ae
ivNetAdmin <bp@ivnet.co.uk>
2016-06-01 11:41:40 +0100

    Generic repository

---
+++ WhenToDig96/WhenToDig96/Data/IRepository.cs
@@ -0,0 +1,29 @@
+﻿
+using SQLite.Net.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace WhenToDig96.Data
+{
+    //public interface IDataSource<T>
+    //{
+    //    Task SaveItem(T item);
+    //    Task DeleteItem(int id);
+    //    Task<T> GetItem(int id);
+    //    Task<ICollection<T>> GetItems(int start = 0, int count = 100, string query = "");
+    //}
+
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> Get();
+        Task<T> Get(int id);
+        Task<List<T>> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null);
+        Task<T> Get(Expression<Func<T, bool>> predicate);
+        AsyncTableQuery<T> AsQueryable();
+        Task<int> Insert(T entity);
+        Task<int> Update(T entity);
+        Task<int> Delete(T entity);
+    }
+}
---
+++ WhenToDig96/WhenToDig96/Data/Repository.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using SQLite.Net.Async;
+using WhenToDig96.Data.Entities;
+
+namespace WhenToDig96.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private SQLiteAsyncConnection _connection;
+
+        public Repository()
+        {
+            _connection = Xamarin.Forms.DependencyService.Get<ISQLite>().GetAsyncConnection();
+            Initialise();
+        }
+
+        private async void Initialise()
+        {
+            await _connection.CreateTableAsync<Job>();
+            await _connection.CreateTableAsync<Plant>();
+            await _connection.CreateTableAsync<Variety>();
+            await _connection.CreateTableAsync<Location>();
+            await _connection.CreateTableAsync<Frost>();
+        }
+
+        public AsyncTableQuery<T> AsQueryable()
+        {
+            return _connection.Table<T>();
+        }
+
+        public async Task<List<T>> Get()
+        {
+            return await _connection.Table<T>().ToListAsync();
+        }
+
+        public async Task<List<T>> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null)
+        {
+            var query = _connection.Table<T>();
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            if (orderBy != null)
+            {
+                query = query.OrderBy<TValue>(orderBy);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await _connection.FindAsync<T>(id);
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return await _connection.FindAsync<T>(predicate);
+        }
+
+        public async Task<int> Insert(T entity)
+        {
+            return await _connection.InsertAsync(entity);
+        }
+
+        public async Task<int> Update(T entity)
+        {
+            return await _connection.UpdateAsync(entity);
+        }
+
+        public async Task<int> Delete(T entity)
+        {
+            return await _connection.DeleteAsync(entity);
+        }
+       
+    }
+}
