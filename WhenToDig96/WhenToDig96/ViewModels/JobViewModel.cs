046c5144ae9ac9c6b3c4e58fbe792855945e62e5
ivNetAdmin <bp@ivnet.co.uk>
2016-06-01 13:15:41 +0100

    JobViewModel

---
+++ WhenToDig96/WhenToDig96/ViewModels/JobViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+using System.ComponentModel;
+
+namespace WhenToDig96.ViewModels
+{
+    public class JobViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public JobViewModel()
+        {
+
+        }
+
+        private string _name;
+        public string Name
+        {
+            set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                }
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+            get
+            {
+                return _name;
+            }
+        }
+    }
+}
